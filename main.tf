08051891295318f5a25da7fc7c6e8b16bd137ed4
Prince Abunku <pabunku@gmail.com>
2025-05-07 23:49:49 +0000

    testing terraform

--- main.tf
+++ main.tf
@@ -9,5 +9,31 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+
 }
+
+# resource "aws_instance" "pipeline-server" {
+#   ami           = "ami-060a84cbcb5c14844"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "PipelineServer"
+#   }
+# }
+
+# resource "aws_vpc" "artist-discovery-vpc" {
+#   cidr_block = "10.0.0.0/16"
+
+#   tags = {
+#     Name = "production"
+#   }
+# }
+
+# resource "aws_subnet" "subnet-1" {
+#   vpc_id     = aws_vpc.artist-discovery-vpc.id
+#   cidr_block = "10.0.1.0/24"
+
+#   tags = {
+#     Name = "prod-subnet"
+#   }
+# }
